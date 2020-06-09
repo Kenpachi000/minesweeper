e1eafd588f475eafce74c880524f9bc6ec837194
Gavin <gavinwu23@gmail.com>
2020-06-08 22:54:42 -0500

    finished createBoard function

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ depending on the userinput
 
 my randomnotes:
     list passed in by argument are mutable and will reflect back
+
     immutable things like int,string will not reflect back
 
     if i pass in an object as a argument to a function, reassignging the variable to something else wont reflect back
@@ -17,53 +18,14 @@ my randomnotes:
 
 #import the Tile class from the Tile.py
 from Tile import Tile
-
-def createTheBord(gameBoard):
-    count = 0
-    for i in range(5):
-        row = []
-        for j in range(6):
-            row.append(Tile(i,j,count))
-            count = count + 1
-        gameBoard.append(row)
-
-def changeValues(TileOBJECT):
-    TileOBJECT.setBombStatus(True) #changes reflect back
-
-    TileOBJECT = 10 #changes wont reflect 
-
+import createGame as cg
 
 
 
 def main ( ):
 
-    t1 = Tile(1,1,1)
-    changeValues(t1)
-    t1.printInfo()
-
-
-
-
-
-    #count = 0
-
-    #below creates a 2d array of Tiles
-    # gameBoard = []
-    # createTheBord(gameBoard)
-
-    # #print out the board in good fashion
-    # for i in range(5):
-    #     for j in range (6):
-    #         print(gameBoard[i][j].ID, ' ', end ='')
-    #     print()
-
-    #test to see if it lines up
-    # gameBoard[3][4].setNumBomb(3)
-    # gameBoard[3][4].printInfo()
-    #
-    # gameBoard[1][1].setBombStatus(True)
-    # gameBoard[1][1].setBlank(True)
-    # gameBoard[1][1].printInfo()
+    gameBoard = cg.createBoard()
+    gameBoard[1][1].printInfo()
 
 
 
--- createGame.py
+++ createGame.py
@@ -6,7 +6,40 @@ This python file will have functions that will be used to create the game in Mai
 -setting up the number of bombs surrounding
 -setting up blank empty tiles
 
-Actions/Commands of the game will be located in another file 
+Actions/Commands of the game will be located in another file
 
 
 """
+from Tile import Tile
+
+#========================================================================================================================
+#function to create the gameboard
+#for now we will hardcode a specific size, later we will ask it as userinput
+#returns back a 2d array of Tiles
+def createBoard():
+    id = 0; #it increments as each tile is made and serves as the id
+    gameBoard = []
+
+    # 2 for loops to create the 2d array of Tiles
+    # tried doing list comprenhension but that didnt work out
+    for i in range(10):
+        row = []
+        for j in range (10):
+            row.append(Tile(i,j,id))
+            id = id + 1
+        gameBoard.append(row)
+
+    return gameBoard
+
+#=============================================================================================================
+# function
+
+
+
+
+
+
+
+
+
+#==============================================================================================================
